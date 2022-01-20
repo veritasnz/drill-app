29e0cda9a2d08f5e204da8ce0f91acc4424de012
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-01-20 17:20:32 +0900

    refactor: remove empty stage-api.ts file

--- src/lib/stage-api.ts
+++
@@ -1,9 +0,0 @@
-import drillData from "../../data/drillData";
-
-import Stage from "../models/Stage.model";
-
-// export const getStageByIndex: (stageIndex: number) => Stage = (
-//     stageIndex: number
-// ) => {
-//     return drillData[stageIndex];
-// };
