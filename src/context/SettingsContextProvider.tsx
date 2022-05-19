b37003732a873a0517ac27b5678f92f72fa72327
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-05-19 14:58:38 +1200

    chore: prepare SettingsContext

--- src/context/SettingsContextProvider.tsx
+++ src/context/SettingsContextProvider.tsx
@@ -1,19 +1,81 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import SettingsContext from "./settings-context";
 
 /** Settings Local Storage Keys */
-enum LSKey {}
+enum LSKey {
+    AUTOPLAY_IS_ON = "autoplay_is_on",
+    SHOW_FURIGANA = "show_furigana",
+    SHOW_ENGLISH = "show_english",
+}
 
 const SettingsContextProvider: React.FC = (props) => {
-    // Load from memory
-    useEffect(() => {}, []);
+    // Toggle booleans
+    const [autoplayIsOn, setAutoplayIsOn] = useState(true);
+    const [showFurigana, setShowFurigana] = useState(true);
+    const [showEnglish, setShowEnglish] = useState(true);
+
+    // Initialize & load from localStorage
+    useEffect(() => {
+        loadBooleanStateValue(LSKey.AUTOPLAY_IS_ON, setAutoplayIsOn);
+        loadBooleanStateValue(LSKey.SHOW_FURIGANA, setShowFurigana);
+        loadBooleanStateValue(LSKey.SHOW_ENGLISH, setShowEnglish);
+    }, []);
+
+    // Create toggle functions
+    const toggleAutoplayIsOn = toggleFnBuilder(
+        LSKey.AUTOPLAY_IS_ON,
+        setAutoplayIsOn
+    );
+    const toggleShowFurigana = toggleFnBuilder(
+        LSKey.SHOW_FURIGANA,
+        setShowFurigana
+    );
+    const toggleShowEnglish = toggleFnBuilder(
+        LSKey.SHOW_ENGLISH,
+        setShowEnglish
+    );
 
     return (
-        <SettingsContext.Provider value={{}}>
+        <SettingsContext.Provider
+            value={{
+                autoplayIsOn,
+                toggleAutoplayIsOn,
+                showFurigana,
+                toggleShowFurigana,
+                showEnglish,
+                toggleShowEnglish,
+            }}
+        >
             {props.children}
         </SettingsContext.Provider>
     );
 };
 
 export default SettingsContextProvider;
+
+/**
+ * Toggle Boolean Helper Functions
+ */
+// Helper function to set Boolean state val from LS given a LSKey
+const loadBooleanStateValue = (
+    lsKey: LSKey,
+    setter: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+    const storedValue = localStorage.getItem(lsKey);
+    if (storedValue === "false") setter(false); // only set to false explicitly
+};
+
+// Builds a toggling function based on the LocalStorage key parameter
+const toggleFnBuilder = (
+    lskey: LSKey,
+    setter: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+    return () => {
+        setter((prevState) => {
+            const newState = !prevState;
+            localStorage.setItem(lskey, newState.toString());
+            return newState;
+        });
+    };
+};
--- src/context/settings-context.tsx
+++ src/context/settings-context.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 
-export interface SettingsContextState {}
+export interface SettingsContextState {
+    autoplayIsOn: boolean;
+    toggleAutoplayIsOn: () => void;
+    showFurigana: boolean;
+    toggleShowFurigana: () => void;
+    showEnglish: boolean;
+    toggleShowEnglish: () => void;
+}
 
-const defaultState: SettingsContextState = {};
+const defaultState: SettingsContextState = {
+    autoplayIsOn: true,
+    toggleAutoplayIsOn: () => {},
+    showFurigana: true,
+    toggleShowFurigana: () => {},
+    showEnglish: true,
+    toggleShowEnglish: () => {},
+};
 
 const SettingsContext = React.createContext<SettingsContextState>(defaultState);
 
