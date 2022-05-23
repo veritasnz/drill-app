2b7e93c3134c39d53803e7cdc9ccbffdb310e86c
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-05-23 22:03:52 +1200

    feat: content update

--- data/drillData.ts
+++ data/drillData.ts
@@ -23,23 +23,16 @@ const drillData: Stage[] = [
                     {
                         id: "yajirobe-long-1",
                         question:
-                            "{自分}(じぶん)_{人}(ひと)?プレゼント?{上}(あ)げる",
-                        answers: [ParticleEnum.KARA],
-                        english: "To give a person a present from yourself",
+                            "{人}(ひと)_プレゼント?{上}(あ)げる",
+                        answers: [ParticleEnum.NI, ParticleEnum.HE],
+                        english: "To give a person a present",
                     },
                     {
                         id: "yajirobe-long-2",
                         question:
-                            "{自分}(じぶん)から{人}(ひと)_プレゼント?{上}(あ)げる",
-                        answers: [ParticleEnum.NI, ParticleEnum.HE],
-                        english: "To give a person a present from yourself",
-                    },
-                    {
-                        id: "yajirobe-long-3",
-                        question:
-                            "{自分}(じぶん)から{人}(ひと)にプレゼント_{上}(あ)げる",
+                            "{人}(ひと)にプレゼント_{上}(あ)げる",
                         answers: [ParticleEnum.WO],
-                        english: "To give a person a present from yourself",
+                        english: "To give a person a present",
                     },
                     {
                         id: "yajirobe-apple",
