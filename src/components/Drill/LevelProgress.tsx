f17d4353bf5cdcc698f63749c066fa889709fb93
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-05-23 22:04:03 +1200

    feat: style LevelProgress

--- src/components/Drill/LevelProgress.tsx
+++ src/components/Drill/LevelProgress.tsx
@@ -7,14 +7,16 @@ interface Props {
 }
 
 const LevelProgress: React.FC<Props> = ({ drillState }) => {
-    let titleText = ``;
-    let progressText = ``;
+    let titleLvl: number | false = false;
+    let titleText: string;
+    let progressText: string;
     let progressPercent = 0;
 
     if (drillState.currentLevel.id === "GRAVEYARD") {
         titleText = drillState.currentLevel.name;
-        progressText = `Remaining questions: ${drillState.currentLevel.questions.length}`;
+        progressText = `Remaining: ${drillState.currentLevel.questions.length}`;
     } else {
+        // Loop until question index
         let questionIndex = 0;
         drillState.currentLevel.questions.every((question, index) => {
             questionIndex = index;
@@ -25,24 +27,29 @@ const LevelProgress: React.FC<Props> = ({ drillState }) => {
             return true;
         });
 
+        // Calculate progressPercent
         progressPercent =
             (questionIndex / drillState.currentLevel.questions.length) * 100;
-        titleText = `Lv. ${drillState.currentLevelNum + 1} - ${
-            drillState.currentLevel.name
-        }`;
+        titleLvl = drillState.currentLevelNum + 1;
+        titleText = drillState.currentLevel.name;
+
+        // Form progressText
         progressText = `${questionIndex} / ${drillState.currentLevel.questions.length}`;
     }
 
     return (
         <header className={s["level-progress"]}>
-            <div>{titleText}</div>
+            <div className={s["level-progress__lvl"]}>
+                {titleLvl && <em>Lv.{titleLvl}</em>}
+                <small>{titleText}</small>
+            </div>
             <div className={s["level-progress__bar-wrap"]}>
                 <span
                     style={{ width: `${progressPercent}%` }}
                     className={s["level-progress__bar"]}
                 ></span>
             </div>
-            <div>{progressText}</div>
+            <div className={s["level-progress__prog"]}>{progressText}</div>
         </header>
     );
 };
