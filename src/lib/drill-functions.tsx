901779e836a0fa955b3527009d95ef75dbe02871
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-05-25 18:27:37 +1200

    style: add documentation for func 'checkAnswerIsCorrect'

--- src/lib/drill-functions.tsx
+++ src/lib/drill-functions.tsx
@@ -1,5 +1,12 @@
 import ParticleEnum from "../models/ParticleEnum.model";
 
+/**
+ * Checks if particle exists (`inputtedAnswer`) within accepted particles array (`correctAnswers`).
+ * Returns boolean representing if the answer is true or false
+ * @param inputtedAnswer
+ * @param correctAnswers
+ * @returns {boolean}
+ */
 export const checkAnswerIsCorrect: (
     inputtedAnswer: ParticleEnum,
     correctAnswers: ParticleEnum[]
